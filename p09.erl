2c0f8a8bd47ad18566f505e74b15b67b9eeeef03
Ivan Kokhan <ivan.kokhan@hotmail.com>
2015-06-09 14:09:10 +0300

    Add changes to one file

--- p09.erl
+++ p09.erl
@@ -7,4 +7,17 @@ pack(L = [_|_]) ->
 pack([]) ->
 	[].
 
-pack([])
+pack([H|T], [], Acc2) ->
+	pack(T, [H], Acc2);
+pack([H|T], [H|_] = Acc1, Acc2) ->
+	pack(T, [H|Acc1], Acc2);
+pack([H|T], Acc1, Acc2) ->
+	pack(T, [H], [Acc1|Acc2]);
+pack([], Acc1, Acc2) ->
+	reverse([Acc1|Acc2], []).
+
+reverse([H|T], Acc3) ->
+	Res = [H] ++ Acc3,
+	reverse(T, Res);
+reverse([], Acc3) ->
+	Acc3.
